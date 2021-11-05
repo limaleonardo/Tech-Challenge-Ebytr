32d1cb81b73b32868a4b04dbe6e03a4f91313ec7
limaleonardo <lima.leonardo.r@gmail.com>
2021-11-05 11:48:17 -0300

    atualiza o readme e remove arquivos de front-end vazios

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import connection from './models/connections.js';
 import taskRouter from './routes/tasksRoutes.js';
 
@@ -6,6 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 app.use('/tasks', taskRouter);
 
